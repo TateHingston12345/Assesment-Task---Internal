229b62778011410330e828e79d7302770408b09a
Tate Hingston <86576306+TateHingston12345@users.noreply.github.com>
2021-08-17 22:21:41 +0000

    unrandomised quiz function

--- main.py
+++ main.py
@@ -8,10 +8,51 @@ def test():
   print("pog")
   # Learn - Function for listing the words and their meanings #
 def learn():
-  print("pog")
+  with open('Quiz.csv', mode='r') as QUIZCSV:
+    csv_reader = csv.reader(QUIZCSV, delimiter=',')
+    for row in csv_reader:
+      if f'{row[0]}' == "Maori":
+        print("Word Definitions:")
+      else:
+        print(f'{row[0]}')
+        if f'{row[5]}' == "A":
+          print(f'{row[1]}')
+        elif f'{row[5]}' == "B":
+          print(f'{row[2]}')
+        elif f'{row[5]}' == "C":
+          print(f'{row[3]}')
+        elif f'{row[5]}' == "D":
+          print(f'{row[4]}')
+        time.sleep(0.25)
   # Scores - Function to show previous scores #
 def scores():
-  print("pog")
+  with open('Quiz.csv', mode='r') as QUIZCSV:
+    csv_reader = csv.reader(QUIZCSV, delimiter=',')
+    correct = 0
+    questions = 0
+    for row in csv_reader:
+      answered = 0
+      if f'{row[0]}' == "Maori":
+        print("Quiz Start:")
+      else:
+        print("What is the english definition of " + f'{row[0]}' + "?")
+        while answered != 1:
+          answer = input("A - " + f'{row[1]}' + ", B - " + f'{row[2]}' + ", D - " + f'{row[3]}' + ", D - " + f'{row[4]}' + ": ")
+          if answer == "A" or answer == "B" or answer == "C" or answer == "D":
+            if answer == f'{row[5]}':
+              print("Correct, good job.")
+              correct = correct + 1
+              questions = questions + 1
+              print("You currently have " + str(correct) + " out of " + str(questions) + ".")
+              answered = 1
+            else:
+              print("Incorrect, the answer was " + f'{row[5]}' + ".")
+              questions = questions + 1
+              print("You currently have " + str(correct) + " out of " + str(questions) + ".")
+              answered = 1
+          else:
+            print("Please enter a valid option.")
+    if questions/correct 
   # Menu - Function to select what to do #
 def menu():
   option = 256
